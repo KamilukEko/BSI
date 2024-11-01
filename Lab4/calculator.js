f2b7a715228c39aa684cb42635e2dcbf12b9da8a
Kamil <kamiluk@onet.pl>
2024-11-01 18:20:08 +0100

    Finished calculator for Lab 4

--- Lab4/calculator.js
+++ Lab4/calculator.js
@@ -1,8 +1,36 @@
 const output = document.getElementById("output");
 output.textContent = "0";
 
-function buttonClicked() {
-      alert("Button clicked");
+
+function addValue(button) {
+      if (output.textContent === "0")
+            output.textContent = "";
+
+      const newOperation = button.querySelector('h1').textContent
+
+      output.textContent += newOperation;
 }
 
-document.addEventListener('click', buttonClicked);
+const standardButtons = document.getElementsByClassName('standard-button');
+Array.from(standardButtons).forEach((button) => {
+
+    button.addEventListener('click', () => {
+
+        addValue(button);
+    });
+});
+
+document.getElementById('clear-button').addEventListener('click', () => {
+
+    output.textContent = "0";
+});
+
+document.getElementById('evaluate-button').addEventListener('click', () => {
+
+    try {
+        output.textContent = eval(output.textContent);
+    }
+    catch (error) {
+        alert(error.textContent);
+    }
+});
