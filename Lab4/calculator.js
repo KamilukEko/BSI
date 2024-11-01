da5a3cf1b470724ba0c94c3a971f6d3cca64fb0d
Kamil <kamiluk@onet.pl>
2024-11-01 17:46:27 +0100

    Updated graphics for Lab 4 (not logic yet)

---
+++ Lab4/calculator.js
@@ -0,0 +1,8 @@
+const output = document.getElementById("output");
+output.textContent = "0";
+
+function buttonClicked() {
+      alert("Button clicked");
+}
+
+document.addEventListener('click', buttonClicked);
