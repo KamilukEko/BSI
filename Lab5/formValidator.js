41dfb80e436b7dbe2f7cb84dd34a8bf7407b1010
Kamil <kamiluk@onet.pl>
2024-11-05 11:31:17 +0100

    Added validation to form for Lab 5

---
+++ Lab5/formValidator.js
@@ -0,0 +1,72 @@
+const errors = [];
+
+function updateInputError(input, containsErrors) {
+
+    if (containsErrors)
+    {
+        if (errors.includes(input.name))
+            return;
+
+        input.previousElementSibling.classList.add("error-label");
+        input.classList.add("error")
+        errors.push(input.name);
+        return;
+    }
+
+    if (!errors.includes(input.name))
+        return;
+
+    input.previousElementSibling.classList.remove("error-label");
+    input.classList.remove("error")
+    errors.remove(input.name);
+}
+
+const phoneNumberRegex = /^(\+\d{1,3}[-.\s]?)?(\(?\d{2,3}\)?[-.\s]?)?(\d{3})[-.\s]?(\d{3})[-.\s]?(\d{3})$/;
+const textRegex = /^[A-Za-ząęłńóśźżĄĘŁŃÓŚŹŻ\s]{1,64}$/;
+const numbersRegex = /^(?:[1-9][0-9]{0,3}|[1-9][0-9]{0,2}[0-9]|[1-9][0-9]{0,1}|[1-9])$/;
+
+function validateInput(input) {
+
+    let containsErrors = false;
+
+    switch (input.getAttribute("validation-type")) {
+        case "phone-number":
+            containsErrors = !phoneNumberRegex.test(input.value);
+            break
+        case "text":
+            containsErrors = !textRegex.test(input.value);
+            break
+        case "number":
+            containsErrors = !numbersRegex.test(input.value);
+            break
+        default:
+            return;
+    }
+
+    updateInputError(input, containsErrors);
+}
+
+const timeouts = {};
+
+const inputs = document.getElementsByTagName("input")
+Array.from(inputs).forEach((input) => {
+
+    if (input.name === undefined)
+        return;
+
+    input.addEventListener("input", () => {
+
+        if (!timeouts.hasOwnProperty(input.name))
+            timeouts[input.name] = null;
+
+        clearTimeout(timeouts[input.name]);
+        timeouts[input.name] = setTimeout(() => validateInput(input), 500)
+    });
+});
+
+document.getElementById('personal-data-form').addEventListener('submit', (e) => {
+    if (errors.length) {
+        e.preventDefault();
+        alert("This form contains errors")
+    }
+});
